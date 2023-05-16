b1a399189e4a6824c02885d521afe858c370951f
Gisele-Silva325 <gisele.silva@sptech.school>
2023-05-16 14:32:24 -0300

    mudança script

--- web-data-viz/site/public/js/funcoes.js
+++ web-data-viz/site/public/js/funcoes.js
@@ -27,17 +27,16 @@ function limparSessao() {
 // carregamento (loading)
 function aguardar() {
     var divAguardar = document.getElementById("div_aguardar");
-    divAguardar.style.display = "flex";
+    divAguardar.innerHTML = `<img src="assets/aguardar.gif" alt="aguardando"></img>`
 }
 
 function finalizarAguardar(texto) {
     var divAguardar = document.getElementById("div_aguardar");
     divAguardar.style.display = "none";
 
-    var divErrosLogin = document.getElementById("div_erros_login");
+    var mensagem_erro = document.getElementById("mensagem_erro");
     if (texto) {
-        divErrosLogin.style.display = "flex";
-        divErrosLogin.innerHTML = texto;
+        mensagem_erro.innerHTML = texto;
     }
 }
 
